d331811e07e20b26d1b9007f04cb259b05f06f8c
Umi Yamaguchi <yamaguchiumi@gmail.com>
2026-08-06 01:35:12 -0400

    fix: prevent narrow viewport overflow

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'master-ten-shell-';
-const CACHE_NAME = `${CACHE_PREFIX}v2`;
+const CACHE_NAME = `${CACHE_PREFIX}v3`;
 
 function scopeUrl(path = './') {
   return new URL(path, self.registration.scope).href;
